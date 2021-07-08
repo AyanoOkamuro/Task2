b96840e9cdf80148936e34d13c950daa63da6b35
AyanoOkamuro <omochi185@gmail.com>
2021-07-08 13:09:11 +0000

    [update5]3-8

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,10 +1,38 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path #投稿詳細ページ(show)へ移動
+      flash[:notice] = "Book was successfully created."
+    else
+      render books_path #投稿一覧ページ(index)へ
+      #flashではない何かerror文が表示されている
+    end
   end
 
   def show
   end
 
   def edit
+    @book = Book.find_by(id: params[:id]) #curent_bookがいる？
+    #validation、なければerror
+    flash = "Book was successfully update"　#successfully必要
   end
+  
+  def destroy
+    @book = Book.find_by(id: params[:id])　#successfully必要
+    
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- bookers/app/models/book.rb
+++ bookers/app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, {presence: true} #titleが存在しないといけない
+  validates :body, {presence: true} #bodyが存在しないと
 end
